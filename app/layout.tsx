ee82e782af3bcd79b5bcc54847acabe3dcfca814
Chris <ccandillo@gmail.com>
2025-03-10 09:50:43 -0600

    Add Google Analytics tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-352BTWNQTE"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-352BTWNQTE');
+            `
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
